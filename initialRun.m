ad48c566a3257aafc0184148428db1a1636af5d5
Wayne Zhao <wayzhao@mit.edu>
2025-08-29 09:41:19 -0400

    Deleted commented out old code

--- initialRun.m
+++ initialRun.m
@@ -1,81 +1,13 @@
 
 %% Initialize
-
-
 % clear; close all; clc;
-% set(0,'DefaultAxesFontSize',16)
-   
-      
-   
-% clear; close all; clc;
-set(0,'defaulttextfontsize',18);set(0,'DefaultAxesFontSize',18);
-% set(0,'defaulttextfontsize',20);set(0,'DefaultAxesFontSize',20);
-set(0,'defaulttextfontsize',14);set(0,'DefaultAxesFontSize',14);
-
-% set(0,'defaulttextfontsize',16);set(0,'DefaultAxesFontSize',16);
-
-set(0,'defaulttextfontsize',25);set(0,'DefaultAxesFontSize',25); %actually these two seem to be coupled, so just do default axes font size
-set(0,'defaulttextfontsize',30);set(0,'DefaultAxesFontSize',30); %actually these two seem to be coupled, so just do default axes font size
 set(0,'defaulttextfontsize',14);set(0,'DefaultAxesFontSize',14);
 
 set(0,'DefaultFigureWindowStyle','docked'); %use when many figures or don't want them always popping up
-% set(0,'DefaultFigureWindowStyle','normal'); %use when publishing in pdf
-% or saving figures automatically
+% set(0,'DefaultFigureWindowStyle','normal'); %use when publishing in pdf or saving figures automatically
 set(0,'defaultFigurePosition',get(0,'screensize'));
-% set(0, 'defaultLineLineWidth', 2)
-% set(0,'DefaultLineMarkerSize',10) 
-
-
-
-set(0, 'defaultLineLineWidth', 4)
-set(0,'DefaultLineMarkerSize',12) 
-
-
-% 
-% 
-% 
-% set(0,'defaulttextfontsize',50);set(0,'DefaultAxesFontSize',50); %actually these two seem to be coupled, so just do default axes font size
-% set(0, 'defaultLineLineWidth', 6)
-% set(0,'DefaultLineMarkerSize',16) 
-% 
-% 
-% 
-% 
-% 
-% 
-% set(0,'defaulttextfontsize',20);set(0,'DefaultAxesFontSize',20); %actually these two seem to be coupled, so just do default axes font size
 set(0, 'defaultLineLineWidth', 2)
-set(0,'DefaultLineMarkerSize',8) 
-
-
+set(0,'DefaultLineMarkerSize',8)
 set(0, 'DefaultAxesTickLength', [0.03, 0.03])
-set(0, 'DefaultAxesLineWidth', 1)
-
-
-
-%% new set for B2
-set(0, 'defaultLineLineWidth', 4)
-set(0,'DefaultLineMarkerSize', 24) 
 set(0,'DefaultAxesFontSize',40);
-set(0, 'DefaultAxesLineWidth', 2)
-
-
-% set(0, 'defaultLineLineWidth', 5)
-% set(0,'DefaultLineMarkerSize', 26) 
-% set(0,'DefaultAxesFontSize', 44);
-% set(0, 'DefaultAxesLineWidth', 3)
-
-
-% % for figures 3 and 7 of paper B2
-% set(0, 'defaultLineLineWidth', 2)
-% set(0,'DefaultLineMarkerSize', 26) 
-% set(0,'DefaultAxesFontSize', 44);
-% set(0, 'DefaultAxesLineWidth', 3)
-
-
-
-%% new set for B1
-set(0, 'defaultLineLineWidth', 4)
-set(0,'DefaultLineMarkerSize', 24) 
-set(0,'DefaultAxesFontSize',37);
-set(0, 'DefaultAxesLineWidth', 2)
+set(0, 'DefaultAxesLineWidth', 1)
